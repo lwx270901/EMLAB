424f55f3c7f47e75f6835ac8af18e8017f8df9cc
lwx270901 <85688900+lwx270901@users.noreply.github.com>
2022-06-18 08:39:22 +0700

    Update preemwithtime.c

--- lab2a/preemwithtime.c
+++ lab2a/preemwithtime.c
@@ -1,1 +1,105 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/FreeRTOSConfig.h"
+#include "freertos/task.h"
+
+
+#define CONFIG_LED_PIN 2
+#define ESP_INTR_FLAG_DEFAULT 0
+#define CONFIG_BUTTON_PIN 0
+
+
+TaskHandle_t ISR = NULL;
+TaskHandle_t task2Handle = NULL;
+TaskHandle_t taskIdleHandle = NULL;
+int count = 0;
+
+void IddleTask(void * pvParameters)
+{
+	while(1)
+	{
+
+			printf("idle \n");
+
+//		printf("idle \n");
+	}
+}
+
+
+void vATask2Function( void *pvParameters )
+{
+	for(;;)
+	{
+		printf("task 2 is running \n");
+	}
+	vTaskDelete(NULL);
+}
+
+
+
+// interrupt service routine, called when the button is pressed
+void IRAM_ATTR button_isr_handler(void* arg) {
+
+ xTaskResumeFromISR(ISR);
+//portYIELD_FROM_ISR(  );
+}
+
+// task that will react to button clicks
+ void button_task(void *arg)
+{
+bool led_status = false;
+ while(1){
+vTaskSuspend(NULL);
+      led_status = !led_status;
+      gpio_set_level(CONFIG_LED_PIN, led_status);
+      printf("Button pressed!!!\n");
+//      printf("task 2 is running \n");
+ }
+}///////////////////////////////////////////////////////////////////
+
+
+
+void app_main()
+{
+
+
+    gpio_pad_select_gpio(CONFIG_BUTTON_PIN);
+    gpio_pad_select_gpio(CONFIG_LED_PIN);
+
+  // set the correct direction
+    gpio_set_direction(CONFIG_BUTTON_PIN, GPIO_MODE_INPUT);
+    gpio_set_direction(CONFIG_LED_PIN, GPIO_MODE_OUTPUT);
+
+  // enable interrupt on falling (1->0) edge for button pin
+    gpio_set_intr_type(CONFIG_BUTTON_PIN, GPIO_INTR_NEGEDGE);
+
+
+  //Install the driver’s GPIO ISR handler service, which allows per-pin GPIO interrupt handlers.
+  // install ISR service with default configuration
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+
+  // attach the interrupt service routine
+    gpio_isr_handler_add(CONFIG_BUTTON_PIN, button_isr_handler, NULL);
+
+
+
+
+    //Create and start stats task
+
+    xTaskCreate( button_task, "button_task", 4096, NULL , 10,&ISR );
+    xTaskCreate( IddleTask, "task idle", 4096, NULL , 0,&taskIdleHandle );// can change to medium to test no time slicing
+    xTaskCreate( vATask2Function, "task 2", 4096, NULL , 0,&task2Handle );
+//    xTaskCreate( IddleTask, "task idle", 4096, NULL , 0,&taskIdleHandle );
+//
+//    while(1)
+//    {
+//    	printf("task 2 is running \n");
+//    	vTaskDelay (1000 / portTICK_RATE_MS ) ;
+//    }
+
+
+
+
+}
 
